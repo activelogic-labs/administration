da0a0225ff1cccd5edd1ac5b0758095ce75bc5e7
Robert Kehoe <robert@activelogiclabs.com>
2016-09-22 09:37:14 -0500

    Removed some arbitrary code

--- src/public/build/js/all-f8642145ff.js
+++ src/public/build/js/all-f8642145ff.js
@@ -29,7 +29,6 @@ $("#main #content .success .dismiss, #main #content .error .dismiss").click(func
     $(this).parent().hide();
     return false;
 }); 
-
 $("[wysiwyg='true']").each(function(index, value)
 {
     var attrId = $(this).attr('id');
@@ -57,40 +56,7 @@ $("[wysiwyg='true']").each(function(index, value)
         ]
     });
 
-    // var editor = CKEDITOR.inline(attrId);
-    //
-    // saveButton.on('click', function(event) {
-    //     console.log("wysiwyg save click");
-    //     var formParams = {};
-    //     formParams[$("#"+attrId).attr('name').replace("_wysiwyg", "")] = editor.getData();
-    //
-    //     $.post(parentForm.attr("action"), formParams, function(response){
-    //
-    //         if (!response.error && parentForm.attr("action").search(response.id) == -1) {
-    //
-    //             updateDetailWithId(response.id);
-    //
-    //         }
-    //
-    //     });
-    // });
-
 });
-
-function updateDetailWithId(id)
-{
-    var form = $("#detailForm");
-    var formAction = form.attr('action');
-
-    var subtitle = $("#subtitle");
-
-    var deleteHref = formAction.replace('save', 'delete');
-    var deleteButton = $("a[href='" + deleteHref + "']");
-
-    form.attr('action', formAction + "/" + id);
-    deleteButton.attr('href', deleteHref + "/" + id);
-    subtitle.html(id);
-}
 /**
  * Created by daltongibbs on 8/22/16.
  */
--- src/resources/assets/js/ckeditor_extend.js
+++ src/resources/assets/js/ckeditor_extend.js
@@ -1,4 +1,3 @@
-
 $("[wysiwyg='true']").each(function(index, value)
 {
     var attrId = $(this).attr('id');
@@ -26,37 +25,4 @@ $("[wysiwyg='true']").each(function(index, value)
         ]
     });
 
-    // var editor = CKEDITOR.inline(attrId);
-    //
-    // saveButton.on('click', function(event) {
-    //     console.log("wysiwyg save click");
-    //     var formParams = {};
-    //     formParams[$("#"+attrId).attr('name').replace("_wysiwyg", "")] = editor.getData();
-    //
-    //     $.post(parentForm.attr("action"), formParams, function(response){
-    //
-    //         if (!response.error && parentForm.attr("action").search(response.id) == -1) {
-    //
-    //             updateDetailWithId(response.id);
-    //
-    //         }
-    //
-    //     });
-    // });
-
 });
-
-function updateDetailWithId(id)
-{
-    var form = $("#detailForm");
-    var formAction = form.attr('action');
-
-    var subtitle = $("#subtitle");
-
-    var deleteHref = formAction.replace('save', 'delete');
-    var deleteButton = $("a[href='" + deleteHref + "']");
-
-    form.attr('action', formAction + "/" + id);
-    deleteButton.attr('href', deleteHref + "/" + id);
-    subtitle.html(id);
-}
